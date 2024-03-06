bb0dc30d40737431e63b74bc6cfac530de661512
Doragon-RH <r216af@icloud.com>
2024-03-06 22:23:40 +0900

    create todo app

---
+++ src/app/page.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { useEffect } from 'react';
+
+import { useRouter } from 'src/routes/hooks';
+
+import { PATH_AFTER_LOGIN } from 'src/config-global';
+
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+// ----------------------------------------------------------------------
+
+export default function Page() {
+  return (
+    <Stack spacing={1} alignItems="center">
+      <Button variant="contained">Button</Button>
+      <Button variant="contained" color="primary">
+        Button
+      </Button>
+      <Button variant="contained" color="secondary">
+        Button
+      </Button>
+      <Button variant="contained" color="info">
+        Button
+      </Button>
+      <Button variant="contained" color="success">
+        Button
+      </Button>
+      <Button variant="contained" color="warning">
+        Button
+      </Button>
+      <Button variant="contained" color="error">
+        Button
+      </Button>
+      <Button disabled variant="contained">
+        Button
+      </Button>
+    </Stack>
+  );
+}
